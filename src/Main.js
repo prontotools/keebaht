7cc6b1be0442443d84970e3c4c169e61fdcdca30
thoritie <thohirh9@gmail.com>
2018-07-22 00:48:15 +0700

    Revise Main style :snowman:

--- src/Main.js
+++ src/Main.js
@@ -4,34 +4,34 @@ import './style.css'
 class Main extends Component {
     render() {
         return(
-            <div class="margin-main">
-                <h1 class="ui center aligned header">Keebaht ?</h1>
-                <div class="ui container">
+            <div className="margin-main">
+                <h1 className="ui center aligned header">Keebaht ?</h1>
+                <div className="ui container shadow">
                     <div class="ui two steps">
                         
-                            <div class="step">
+                        <div className="step">
                                 <a href="/Billing">
-                                    <div class="content">
-                                        <div class="title">Billing</div>
-                                        <div class="description">Enter billing information</div>
+                                    <div className="content">
+                                    <img src="https://image.flaticon.com/icons/svg/942/942803.svg" width="50px;"/>
+                                        <div className="title">Billing</div>
+                                        <div className="description">Enter billing information</div>
                                     </div>
                                 </a>
-                            </div>
-                      
-                        
-                        <div class="step">
+                        </div>
+
+                        <div className="step">
                             <a href="/Amount">
-                                <div class="content">
-                                    <div class="title">Keebaht</div>
-                                    <div class="description">Show you have to pay Keebaht</div>
+                                <div className="content">
+                                <img src="https://image.flaticon.com/icons/svg/945/945971.svg" width="50px;"/>
+                                    <div className="title">Keebaht</div>
+                                    <div className="description">Show you have to pay Keebaht</div>
                                 </div>
                             </a>
+                        </div> 
                         </div>
-                        
-        
+                       
                     </div>
-            </div>
-            </div>
+                </div>
         )
     }
 }
