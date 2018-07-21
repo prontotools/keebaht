2deeab050b22ebdc2a3d872903627c1d454b1ab8
zkan <kan@prontomarketing.com>
2018-07-21 23:23:44 +0700

    Use App as root :bear:

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-//import App from './App'
-import Billing from './Billing'
+import App from './App'
 import 'semantic-ui-css/semantic.min.css'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<Billing />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
