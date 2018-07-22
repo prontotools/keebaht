61ec0f5c2c11f3bdc88294634ee00986ab6e58f6
saowaluck <pop@prontomaarketing.com>
2018-07-22 12:10:26 +0700

    Revise ui in summary billing

--- src/Summary.js
+++ src/Summary.js
@@ -1,56 +1,73 @@
 import React, { Component } from 'react'
 import { db } from './firebase'
+import Main from './Main'
+
 import './style.css'
 
 class Summary extends Component {
-    state = {
-        payers: [],
-    }
+  state = {
+    payers: [],
+  }
 
-    componentDidMount() {
-        db.collection('payers').get().then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                this.setState(prevState => ({
-                    payers: [{
-                        username: doc.data().name,
-                        amount: doc.data().amount
-                    }, ...prevState.payers
-                    ]
-                }))
-            })
-        })
-    }
+  componentDidMount() {
+    db.collection('payers').get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        this.setState(prevState => ({
+          payers: [{
+            username: doc.data().name,
+            amount: doc.data().amount,
+            menu: doc.data().menu
+          }, ...prevState.payers
+          ]
+        }))
+      })
+    })
+  }
 
-    render() {
-        return (
-            <div className="margin-main">
-                <h1 className="ui center aligned header">กี่บาท?</h1>
-                <div className="ui container">
-                    <div class="ui three column grid stackable">
-                        {
-                            this.state.payers.map(payer =>
-                                <div className="ui column">
-                                    <div class="ui card">
-                                        <div class="content">
-                                            <div class="center aligned header">{payer.username}</div>
-                                            <div class="center aligned description">
-                                                <p></p>
-                                            </div>
-                                        </div>
-                                        <div class="extra content">
-                                            <div class="right floated">
-                                                {payer.amount}฿
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            )
-                        }
+  render() {
+    console.log(this.state.payers)
+    return (
+      <div className="margin-main">
+        <h1 className="ui center aligned header">กี่บาท?</h1>
+        <div className="ui container">
+          <div class="ui three column doubling stackable grid container">
+            {
+              this.state.payers.map(payer =>
+                <div className="column">
+                  <div class="shadow ui card link card-section fluid">
+                    <div class="content">
+                      <h3 class="pay-username">{payer.username.toUpperCase()}</h3>
+                      <div class="ui description">
+                        <div class="ui divided list">
+                          {payer.menu.map(item => (
+                            <div class="item">
+                              <div class="right floated content">
+                                <div class="meta">{item.total} x {item.amount} = {item.total * item.amount}</div>
+                              </div>
+                              <div class="meta">
+                                {item.name}
+                              </div>
+                            </div>
+                          ))}
+
+                        </div>
+
+                      </div>
                     </div>
+                    <div class="extra content">
+                      <div class="right floated price">
+                        {payer.amount}฿
+                      </div>
+                    </div>
+                  </div>
                 </div>
-            </div>
-        )
-    }
+              )
+            }
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Summary
