86b61675c51d509a80dc5f6df8eca45f856fa52a
zkan <kan@prontomarketing.com>
2018-07-22 07:17:54 +0700

    Adjust spacing :bear:

--- src/Summary.js
+++ src/Summary.js
@@ -2,38 +2,38 @@ import React, { Component } from 'react'
 import { db } from './firebase'
 
 class Summary extends Component {
-    state = {
-        payers: [],
-    }
+  state = {
+    payers: [],
+  }
 
-    componentDidMount() {
-        db.collection('payers').get().then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                this.setState(prevState => ({
-                    payers: [{
-                        username: doc.data().name,
-                        amount: doc.data().amount
-                    }, ...prevState.payers
-                    ]
-                }))
-            });
-        });
-    }
+  componentDidMount() {
+    db.collection('payers').get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        this.setState(prevState => ({
+          payers: [{
+            username: doc.data().name,
+            amount: doc.data().amount
+            }, ...prevState.payers
+          ]
+        }))
+      })
+    })
+  }
 
-    render() {
-        return (
-            <div>
-                <h1>KeeBath?</h1>
-                {
-                    this.state.payers.map(payer => (
-                        payer.username === this.props.username ?
-                            <h1>{payer.username}{payer.amount}</h1> :
-                            <li>{payer.username}{payer.amount}</li>
-                    ))
-                }
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>KeeBath?</h1>
+        {
+          this.state.payers.map(payer => (
+            payer.username === this.props.username ?
+              <h1>{payer.username}{payer.amount}</h1> :
+              <li>{payer.username}{payer.amount}</li>
+          ))
+        }
+      </div>
+    )
+  }
 }
 
 export default Summary
